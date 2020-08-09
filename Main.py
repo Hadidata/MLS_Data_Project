02bf4138297b1d585d144a9ca5e75d0c15f58f5f
gjk9425 <georgejoshua94@gmail.com>
2020-08-09 17:44:59 -0600

    adding main_file

---
+++ Main.py
@@ -0,0 +1,11 @@
+#This module is the Main python file that calls all other python files/classes that interact with the SQL Database
+
+test_array = ["https://www.livrealestate.ca/idx/1020-9-ave-se-calgary-ab-t2g-0s7/13050739_spid/","https://www.livrealestate.ca/idx/2429-25-st-sw-calgary-ab-t3e-1x5/10948220_spid/","https://www.livrealestate.ca/idx/230-39-ave-sw-calgary-ab-t2s-0w5/12106898_spid/"]
+
+
+def main():
+    print("test")
+
+
+if __name__== "__main__":
+    main()
